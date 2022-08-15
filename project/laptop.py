ea90c64e1e3f65dcc4566e4dd88988ff9ea9ae52
nxfive <95922573+nxfive@users.noreply.github.com>
2022-08-15 23:13:40 +0200

    change __str__ magic method return, it shows less information

--- project/laptop.py
+++ project/laptop.py
@@ -1,18 +1,17 @@
 import random
-from product import Product
+from project.product import Product
 
 
 class Laptop(Product):
 
-    def __init__(self, brand, name, net_price):
-        super().__init__(brand, name, net_price)
+    def __init__(self, brand, prod_name, net_price):
+        super().__init__(brand, prod_name, net_price)
         self._ram_memory = Laptop._add_ram_memory()
         self._built_in_memory = Laptop._add_built_in_memory()
         self._type_drive = Laptop._add_type_drive()
 
     def __str__(self):
-        return f'{super().__str__()}, Price: {self.price_with_margin}\nRAM memory: {self.ram_memory}\n' \
-               f'Built-in memory: {self.built_in_memory}\nType of drive: {self.type_drive}'
+        return f'{super().__str__()}, net_price: {self.net_price}'
 
     @property
     def ram_memory(self):
--- project/phone.py
+++ project/phone.py
@@ -1,22 +1,21 @@
 import random
-from product import Product
+from project.product import Product
 
 
 class Phone(Product):
 
-    def __init__(self, brand, name, net_price):
-        super().__init__(brand, name, net_price)
+    def __init__(self, brand, prod_name, net_price):
+        super().__init__(brand, prod_name, net_price)
         self._is_camera = Phone._choice_for_camera()
-        self._is_keyboard = Phone._choice_option()
+        self._is_keyboard = random.choice(('YES', 'NO'))
         if self._is_keyboard == 'NO':
             self._is_touch_screen = 'YES'
         else:
-            self._is_touch_screen = Phone._choice_option()
+            self._is_touch_screen = random.choice(('YES', 'NO'))
         self._memory = Phone._add_memory()
 
     def __str__(self):
-        return f'{super().__str__()}\nCamera: {self.is_camera}\nKeyboard: {self.is_keyboard}' \
-               f'\nTouch screen: {self.is_touch_screen}\nMemory: {self.memory}'
+        return f'{super().__str__()}, net_price: {self.net_price}'
 
     @property
     def is_camera(self):
@@ -33,10 +32,6 @@ class Phone(Product):
     @property
     def memory(self):
         return self._memory
-
-    @staticmethod
-    def _choice_option():
-        return random.choice(['YES', 'NO'])
 
     @staticmethod
     def _add_memory():
