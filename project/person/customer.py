1bb0381419b55e707da0d0f1ee39b97ee38d04a1
nxfive <95922573+nxfive@users.noreply.github.com>
2022-08-21 23:29:57 +0200

    make all attrs private and to city, entry_time, shop, shopping_cart add property

--- project/person/customer.py
+++ project/person/customer.py
@@ -9,20 +9,36 @@ class Customer(Person):
 
     def __init__(self, shop_instance, name, surname, city, money):
         super().__init__(name, surname)
-        self.email = f'{self.name}.{self.surname}{str(self.identity)[-3:]}@mail.com'
-        self.city = city
-        self.entry_time = time.strftime('%H:%M:%S %d.%m.%Y', time.localtime())
-        self.money = money
-        self.shop = shop_instance
-        self.shopping_cart = ShoppingCart()
-        self.is_paid = False
-        self.exit_time = None
-        self.bill = None
+        self._email = f'{self.name}.{self.surname}{str(self.identity)[-3:]}@mail.com'
+        self._city = city
+        self._entry_time = time.strftime('%H:%M:%S %d.%m.%Y', time.localtime())
+        self._money = money
+        self._shop = shop_instance
+        self._shopping_cart = ShoppingCart()
+        self._is_paid = False
+        self._exit_time = None
+        self._bill = None
         Person.customers.append(self)
         insert_data(self, database, 'customers')
 
     def __str__(self):
         return f'{super().__str__()}, ENTRY TIME: {self.entry_time}'
+
+    @property
+    def city(self):
+        return self._city
+
+    @property
+    def entry_time(self):
+        return self._entry_time
+
+    @property
+    def shop(self):
+        return self._shop
+
+    @property
+    def shopping_cart(self):
+        return self._shopping_cart
 
     def make_action(self, count):
         for i in range(count):
@@ -33,23 +49,22 @@ class Customer(Person):
     def pay_for_items(self):
         time.sleep(random.randint(1, 5))
         total = 0
-        while self.is_paid is False:
+        while self._is_paid is False:
             for product in self.shopping_cart.items:
                 total += product.price_with_margin
-            if self.money >= total:
+            if self._money >= total:
                 for product in self.shopping_cart.items:
                     database.update_product('products', product.identity)
                     insert_data(product, database, 'sold')
-                self.money -= total
-                self.is_paid = True
-                self.bill = total
+                self._money -= total
+                self._is_paid = True
+                self._bill = total
                 Customer.bye(self)
             else:
                 self.shopping_cart.remove_product(self.shopping_cart.items[-1])
                 total = 0
 
     def bye(self):
-        self.exit_time = time.strftime('%H:%M:%S %d.%m.%Y', time.localtime())
-        print(f'{self}, LEFT AT TIME: {self.exit_time}')
+        self._exit_time = time.strftime('%H:%M:%S %d.%m.%Y', time.localtime())
+        print(f'{self}, LEFT AT TIME: {self._exit_time}')
         database.update_customer('customers', self)
-
