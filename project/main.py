67298735bd5c38128cef0e38aeaffa81087a5c83
nxfive <95922573+nxfive@users.noreply.github.com>
2022-08-24 19:14:25 +0200

    change range to (0, 5) to avoid IndexError

--- project/main.py
+++ project/main.py
@@ -16,7 +16,7 @@ _data = [
 
 
 def create_db(item):
-    db = Database(f"{item}.db")
+    db = Database(f"{item.lower()}.db")
     create_database(db)
     return db
 
@@ -28,18 +28,21 @@ async def create_process(number):
         customer = Customer(shop, *(create_customer()))
         customer.make_action(random.randint(1, 5))
     revenue = 0
-    for product in shop._sold_products:
+    for product in shop._sold_products_by_instance:
         revenue += product.price_with_margin - product.net_price
-    sold = len(shop._sold_products)
-    print(f'Shop sold {len(shop._sold_products)} products, earned: {revenue}')
+    sold = len(shop._sold_products_by_instance)
+    earned_all = [item.price_with_margin - item.net_price for item in shop._sold_products_all]
+    print(f'Shop {shop.name} sold {sold} products, earned: {revenue}')
+    print(f'All shops sold {len(shop._sold_products_all)} products, earned: {sum(earned_all)}')
     return shop.name, revenue, sold
 
 
 async def main():
-    zero_task = list(asyncio.all_tasks())[0]
-    for r in range(1, 6):
-        task = asyncio.create_task(create_process(r))
-    tasks = asyncio.all_tasks() - {zero_task}
+    # zero_task = list(asyncio.all_tasks())[0]
+    tasks = []
+    for r in range(0, 5):
+        tasks.append(asyncio.create_task(create_process(r)))
+    # tasks = asyncio.all_tasks() - {zero_task}
     returns = await asyncio.gather(*tasks, return_exceptions=True)
     print(returns)
 
