28701b7bf3154c0e820ede644b634bffb94b6c74
nxfive <95922573+nxfive@users.noreply.github.com>
2022-08-24 02:06:54 +0200

    add asyncio to manage 5 other shops and databases add func create_db() and create_process()

--- project/main.py
+++ project/main.py
@@ -1,20 +1,46 @@
+import asyncio
+
 from project.shop.shop import Shop
 from project.person.customer import Customer
 from project.utils.utils import create_customer
+from project.database.database import *
 import random
 
+_data = [
+    ('Electronic', 20, 5),
+    ('MediaMax', 40, 8),
+    ('ExpertRTV', 32, 6),
+    ('ComputerM', 25, 6),
+    ('TeleMarkt', 30, 4),
+]
 
-def main():
-    shop = Shop('Electronic', 20, 5)
-    for i in range(10):
+
+def create_db(item):
+    db = Database(f"{item}.db")
+    create_database(db)
+    return db
+
+
+async def create_process(number):
+    shop = Shop(create_db(_data[number][0]), *(_data[number]))
+
+    for i in range(5):
         customer = Customer(shop, *(create_customer()))
         customer.make_action(random.randint(1, 5))
     revenue = 0
     for product in shop._sold_products:
         revenue += product.price_with_margin - product.net_price
-
+    sold = len(shop._sold_products)
     print(f'Shop sold {len(shop._sold_products)} products, earned: {revenue}')
+    return shop.name, revenue, sold
 
 
-if __name__ == '__main__':
-    main()
+async def main():
+    zero_task = list(asyncio.all_tasks())[0]
+    for r in range(1, 6):
+        task = asyncio.create_task(create_process(r))
+    tasks = asyncio.all_tasks() - {zero_task}
+    returns = await asyncio.gather(*tasks, return_exceptions=True)
+    print(returns)
+
+asyncio.run(main())
